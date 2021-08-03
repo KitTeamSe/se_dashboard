ebc25e3a7408eb22561eccecc364fbd73bb59f39
hwanfront <kim015jh@gmail.com>
2021-08-04 01:55:07 +0900

    [ADD] 토큰 만료 시 에러 처리 추가

--- src/libs/api/Manage/account.js
+++ src/libs/api/Manage/account.js
@@ -51,6 +51,10 @@ export const updateAccount = ({
   };
 
   return client.put(`${URL}`, parameter, tokenHeader(token)).catch(error => {
+    if (error.response.data.code === 'GE05') {
+      localStorage.clear();
+      window.location.reload(true);
+    }
     throw error.response.data;
   });
 };
